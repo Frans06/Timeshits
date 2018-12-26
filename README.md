15557731e62a2ba8329bb98c0b6715534c5499d3
frans <franscaraveli@gmail.com>
2018-12-26 09:21:55 -0600

    Firscomit

---
+++ README.md
@@ -0,0 +1,7 @@
+***INSTALL
+`pipenv install`
+Modify json on main file
+
+***RUN
+
+`python main.py`
---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+if __name__ == '__main__':
+    '''
+    name: Nombre completo
+    status7: Proyecto{5:"Otro(especificar)"}
+    numbers: Horas Invertidas en la Semana
+    date2: Fecha Fin
+    date: Fecha inicio
+    '''
+    json={"answers":{
+            "name":"Frans Ramirez Neyra",
+            "status7":"5","text99":"Solquim",
+            "numbers":"45",
+            "date2":"2018-12-27",
+            "date":"2018-12-24"}}
+    headers={
+        'Content-Type': "application/json",
+        'cache-control': "no-cache"
+        }
+    r = requests.post('https://forms.monday.com/forms/a48d63c0764f740996ff1b90e496b53a/submit_form', json=json, headers=headers)
+    print('Response:')
+    print(r.status_code)
+    print('Done')
